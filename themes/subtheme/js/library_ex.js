f7e4508faf3472dafcf2c7aa6d26fd1267dd2862
jollymugivara <59286915+jollymugivara@users.noreply.github.com>
2020-03-10 15:15:44 +0200

    Add files via upload

--- themes/subtheme/js/library_ex.js
+++ themes/subtheme/js/library_ex.js
@@ -16,17 +16,45 @@ $('header.navbar').removeClass("glide");
 });
 })(jQuery);
 
+(function ($) {
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1000){
+$('#about').addClass("active").removeClass("active");
+}
+else{
+$('.navbar-header ul li a').removeClass("active");
+}
+});
+})(jQuery);
 
 (function ($) {
 $(window).scroll(function () {
-  $(".region-header").each(function (index) {
+  $("#navigation_lines").each(function (index) {
     let id = $(this).attr("id");
-    if ($(this).offset().top - 300 < $(window).scrollTop()) {
-      $('.nav-line[href="#' + id + '"]')
-        .addClass("active")
-        .siblings()
-        .removeClass("active");
+    if ($(this).offset().top - 600 < $(window).scrollTop()) {
+      switch (index) {
+        case 0:
+          $(".nav-line").removeClass(
+            "white", 1000
+          );
+          break;
+        case 1:
+          $(".nav-line").addClass(
+            "white", 1000
+          );
+          break;
+        case 2:
+          $(".nav-line").addClass(
+            "white", 1000
+          ); м
+          break;
+        case 3:
+          $(".nav-line").removeClass(
+            "white", 1000
+          );
+          break;
+      }
     }
   });
 });
-});
+})(jQuery);
